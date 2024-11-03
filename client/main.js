34985d1b71c4d648e7ae581ed5f905721618445c
hunzooyun <hunzooyun@gmail.com>
2024-11-03 19:31:37 +0900

    초기 구성 및 과제 1차 작성

---
+++ client/main.js
@@ -0,0 +1,7 @@
+const hello = 'hello';
+
+// alert('경고창!!!');
+
+// confirm('정말.. 지울거야?...');
+
+// prompt('당신은 잘생겼습니까?')
---
+++ mission01/core/mission.js
@@ -0,0 +1,28 @@
+let person;
+
+function getValueAtObject(obj, key) {
+  if (obj.getValueAtObject(key)) {
+    return obj[key];
+  } else {
+    return 'error: 존재하지 않는 값입니다. 다시 입력해주세요.';
+  }
+}
+
+console.log(getValueAtObject(person, 'name'));
+console.log(getValueAtObject(person, 'age'));
+console.log(getValueAtObject(person, 'city'));
+console.log(getValueAtObject(person, 'country'));
+
+function getNumberAtArray(arr, index) {
+  if (index >= 0 && index < arr.length) {
+    return arr[index];
+  } else {
+    return "error: '배열 밖의 범위의 값입니다'.";
+  }
+}
+const numbers = [10, 20, 30, 40, 50];
+
+console.log(getNumberAtArray(numbers, 2));
+console.log(getNumberAtArray(numbers, 4));
+console.log(getNumberAtArray(numbers, 5));
+console.log(getNumberAtArray(numbers, -1));
